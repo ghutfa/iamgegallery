905c59aad89a58fa395b0ee4b4ef1e68c0f95ee6
ghutfa <111077873+ghutfa@users.noreply.github.com>
2023-02-24 18:51:24 -0500

    Add files via upload

---
+++ project.py
@@ -0,0 +1,25 @@
+import os
+import cv2
+import time
+path = "images/"
+images = []
+for file in os.listdir(path):
+    name, ext = os.path.splitext(file)
+
+    if ext in ['.gif', '.png', '.jpg', '.jpeg','.jfif']:
+        file_name = path+"/"+file
+        print(file_name)
+        images.append(file_name)
+count = len(images)
+frame = cv2.imread(images[0])
+height,width,channels = frame.shape
+size = (width,height)
+print(size)
+
+fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+out = cv2.VideoWriter("friendship.avi", fourcc, 3 , (width, height))
+for i in range(0,count-1):
+    img = cv2.imread(images[i])
+    out.write(img)
+out.release()
+# fixed bug
